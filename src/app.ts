5e8d11c85fd6a2821ad098fac45f166b0ba8249d
khairul <md.raju130@yahoo.com>
2023-09-19 17:15:42 +0600

    err

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application } from 'express'
 import cors from 'cors'
 
-const app: Application = express()
+const app: appli = express()
             app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))

01ec5e84ad667dcd38fa7e322f6954b3c800d38a
khairul <md.raju130@yahoo.com>
2023-09-21 23:58:08 +0600

    add

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application } from 'express'
 import cors from 'cors'
 
-const app: Apsp = express()
+const app: Apspd = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))

4e712398f439b2fb5fbbe250cc51ceeae0d4f2e6
khairul <md.raju130@yahoo.com>
2023-09-22 08:25:05 +0600

    sucessfully

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application } from 'express'
 import cors from 'cors'
 
 const app: Application = express()
-            app.use(express.json())
+app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
